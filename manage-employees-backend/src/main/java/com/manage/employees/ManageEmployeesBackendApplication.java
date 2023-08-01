d380ed04e4e7fd141540931cb7c17488a02fd2fa
yenni delgado <yennivanessadelgado@gmail.com>
2023-08-01 11:04:02 -0500

    My first commit

---
+++ manage-employees-backend/src/main/java/com/manage/employees/ManageEmployeesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.manage.employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageEmployeesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageEmployeesBackendApplication.class, args);
+	}
+
+}
